1106d61fb8af7e75f5f801c2787b72220c416c73
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-07-18 14:32:39 -0500

    Se me olvido esta xd

--- src/main/java/co/edu/uniquindio/alquiler/model/Tripulante.java
+++ src/main/java/co/edu/uniquindio/alquiler/model/Tripulante.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.alquiler.model;
 
 
+import co.edu.uniquindio.alquiler.Listas.ListaSimple;
 import lombok.*;
 
 @Getter
@@ -9,4 +10,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Tripulante extends Persona {
+
+    private String ocupacion;
+    private ListaSimple<EstudioRealizado> estudiosRealizados;
 }
